0f3162e006b397a466c86baea31520b8b9e5bfa1
Yangzai <Yangzai@163.com>
2022-08-31 21:04:56 +0800

    second commit

--- src/main/java/com/yangzai/git/git_test.java
+++ src/main/java/com/yangzai/git/git_test.java
@@ -3,5 +3,6 @@ package com.yangzai.git;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("f**k you");
     }
 }
