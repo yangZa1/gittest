03fe0cce76af83a5355e43e1e8fae66af55cea2c
yangZa1 <92801807+yangZa1@users.noreply.github.com>
2022-08-31 21:48:16 +0800

    Update git_test.java

--- src/main/java/com/yangzai/git/git_test.java
+++ src/main/java/com/yangzai/git/git_test.java
@@ -4,5 +4,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("f**k you");
+        System.out.println("you f**k me ? ");
+        
     }
 }
